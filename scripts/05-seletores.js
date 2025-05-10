cd5b6c6897ee4b21818f6101cb5e0a3176b29c5b
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2025-05-10 10:28:09 -0300

    Aula 2025-05-10

--- scripts/05-seletores.js
+++ scripts/05-seletores.js
@@ -1,3 +1,12 @@
+let gifsDisponiveis = [
+    "https://media.giphy.com/media/YBHJyPCU9h1VewdaPZ/giphy.gif",
+    "https://media.giphy.com/media/7whm31rMz0t9yB42Tp/giphy.gif",
+    "https://media.giphy.com/media/yALcFbrKshfoY/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3cyZTNmZXhwdThjZ2doejkxZjBoN3F1amRyM215M3p6MTdsczgzbyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/G1vplGMypxBcp7kx32/giphy.gif",
+    "https://media.giphy.com/media/e6TR9n00dL3JS/giphy.gif",
+    "https://media.giphy.com/media/BdAn5S0xigpO/giphy.gif"
+]
+
 // Selecionar um elemento por tag
 let a = document.querySelector("a");
 
@@ -48,3 +57,114 @@ function alterarVisibilidadeTagP() {
             paragrafo.style.display = "block";
     });
 }
+
+function alterarTamanhoCaixa(){
+    let divCaixas = document.getElementsByName("caixa");
+    
+    if (divCaixas.length === 0){
+        alert("Div da caixa não encontrada com o nome 'caixa'.")
+        return;
+    }
+
+    // Pegar o primeiro elemento da lista de elementos
+    let divCaixa = divCaixas[0];
+    // Pegar do elemento(div) os estilos que estão aplicados naquele instante
+    let tamanhoCaixa = window.getComputedStyle(divCaixa);
+    
+    let largura = parseInt(tamanhoCaixa.width, 10);
+    let altura = parseInt(tamanhoCaixa.height, 10);
+    
+    let novaLargura = largura + 20;
+    let novaAltura = altura + 20;
+
+    // Alterar o width e height da div de 20 em 20
+    divCaixa.style.width = novaLargura + "px";
+    divCaixa.style.height = novaAltura + "px";
+}
+
+function criarGif(){
+    let divs = document.getElementsByClassName("gifs");
+    if (divs.length === 0){
+        alert("Não foi possível criar o gif")
+        return
+    }
+
+    let divGifs = divs[0];
+
+    let indiceAleatorio = Math.floor(Math.random() * 6)
+    let gifAleatorio = gifsDisponiveis[indiceAleatorio];
+
+    let imagem = `<img src="${gifAleatorio}">`
+    
+    divGifs.innerHTML += imagem;
+}
+
+function adicionarAtalho(e){
+    // Alt + G 
+    const tecla = e.key.toLowerCase();
+    if(e.altKey && !e.shiftKey){
+        switch(tecla){
+            case "g": // Alt + G
+                criarGif();
+                break;
+            case "arrowup": // Alt + ↑
+                movimentar("cima");
+                break;
+            case "arrowdown": // Alt + ↓
+                movimentar("baixo");
+                break;
+            case "arrowleft": // Alt + ←
+                movimentar("esquerda");
+                break;
+            case "arrowright": // Alt + →
+                movimentar("direita");
+                break;
+        }
+        // Encerra a execução daquele atalho
+        e.preventDefault();
+    }
+}
+
+function movimentar(direcao){
+    let divCaixas = document.getElementsByName("caixa");
+    if (divCaixas.length === 0){
+        alert("Div da caixa não encontrada com o nome 'caixa'.")
+        return;
+    }
+
+    // Pegar o primeiro elemento da lista de elementos
+    let divCaixa = divCaixas[0];
+    // Pegar do elemento(div) os estilos que estão aplicados naquele instante
+    let estiloComputado = window.getComputedStyle(divCaixa);
+
+    let topAtual = parseInt(estiloComputado.top, 10);
+    let leftAtual = parseInt(estiloComputado.left, 10);
+
+    let novoTop = topAtual;
+    let novoLeft = leftAtual;
+    const passo = 10;
+    // window.innerHeight é altura da janela
+    switch(direcao){
+        case "cima": novoTop -= passo; break;
+        case "baixo": novoTop += passo; break;
+        case "direita": novoLeft += passo; break;
+        case "esquerda": novoLeft -= passo; break;
+    }
+
+    divCaixa.style.top = novoTop + "px";
+    divCaixa.style.left = novoLeft + "px";
+}
+
+document.addEventListener("keyup", adicionarAtalho);
+
+/*
+Exercícios:
+Ex. 1: Criar uma página com os seguintes elementos:
+- Label referenciando o id nome-pet ex.: for='idade-cliente'
+- Campo com o id nome-pet ex.: <input type="number" id="idade-cliente">
+- Criar um botão Obter por Id: que utiliza o getElementById e obtem o valor do campo digitado
+    Apresentando para o usuário com alert
+    Depois vc deve trocar cor da fonte do campo
+ 
+
+*/
